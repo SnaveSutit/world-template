4588c84c859ebe2d43c28309129d105bac379dbc
SnaveSutit <snavesutit@gmail.com>
2022-02-06 11:10:34 -0500

    commit basic

---
+++ datapack/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	global: {
+		onBuildSuccess: null,
+	},
+	mc: {
+		dev: true,
+		header: '# Developed by SnaveSutit\n# built using mc-build (https://github.com/mc-build/mc-build)',
+		internalScoreboard: 'i',
+		generatedDirectory: 'zzz',
+		rootNamespace: null,
+	},
+}
