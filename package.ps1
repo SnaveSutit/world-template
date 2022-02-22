1a6f1a0b1cf69298016c884d552418e6f3e917bd
SnaveSutit <snavesutit@gmail.com>
2022-02-22 09:54:49 -0500

    Update package.ps1

--- package.ps1
+++ package.ps1
@@ -1,23 +1,25 @@
 
 # Remove old dist folder and it's contents if it exists
 if (Test-Path -Path './dist') {
-	Remove-Item -Path './dist/' -Recurse -Force
+	Remove-Item -Path './dist/' -Recurse -Force | Out-Null
 }
 # Create new dist folder
-New-Item -ItemType Directory -Path "./dist/" -Force
+New-Item -ItemType Directory -Path "./dist/" -Force | Out-Null
 # Make sure the .temp folder exists
 if (-not(Test-Path -Path './.temp')) {
-	New-Item -ItemType Directory -Path "./.temp/" -Force
+	New-Item -ItemType Directory -Path "./.temp/" -Force | Out-Null
 }
 # Copy the world into the temp folder to prevfent accidentally breaking things
-Copy-Item -Path './world/' -Destination './.temp/' -Recurse
+Copy-Item -Path './world/' -Destination './.temp/' -Recurse | Out-Null
+# Compress the data pack and output it
+Compress-Archive -Path './datapack/*' -DestinationPath './dist/datapack.zip' | Out-Null
 # Compress the resource pack and output it
-Compress-Archive -Path './resources/*' -DestinationPath './dist/resources.zip'
+Compress-Archive -Path './resources/*' -DestinationPath './dist/resources.zip' | Out-Null
 # Copy the compressed resource pack into the world
-Copy-Item -Path './dist/resources.zip' -Destination './.temp/world/' -Recurse
+Copy-Item -Path './dist/resources.zip' -Destination './.temp/world/' -Recurse | Out-Null
 # Compress the world and output it
-Compress-Archive -Path './.temp/world/*' -DestinationPath './dist/world.zip'
+Compress-Archive -Path './.temp/world/*' -DestinationPath './dist/world.zip' | Out-Null
 # Remove temporary files
-Remove-Item -Path './.temp/' -Recurse -Force
+Remove-Item -Path './.temp/' -Recurse -Force | Out-Null
 
 Write-Host "Successfully packaged world and resource pack!"
