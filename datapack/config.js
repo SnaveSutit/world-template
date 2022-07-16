75b8e7c4c8906e5342f4e7e322c09b050ab1da46
SnaveSutit <snavesutit@gmail.com>
2022-07-15 20:07:47 -0400

    Fixes

--- datapack/config.js
+++ datapack/config.js
@@ -3,8 +3,7 @@ const { exec } = require('child_process')
 module.exports = {
 	global: {
 		onBuildSuccess: (build) => {
-			if (build) {
-				console.log('Build: ', build)
+			if (build.config.mc.package_after_build) {
 				exec(
 					`".\\package.ps1"`,
 					{ shell: 'powershell.exe' },
@@ -21,6 +20,8 @@ module.exports = {
 	},
 	mc: {
 		dev: true,
+		// Whether or not to compile the packaged version of the world after the build process is complete.
+		package_after_build: false,
 		header: '# Developed by SnaveSutit (Discord: SnaveSutit#0042 | Email: SnaveSutit@gmail.com)\n# built using mc-build (https://github.com/mc-build/mc-build)',
 		internalScoreboard: 'i',
 		generatedDirectory: 'zzz',
