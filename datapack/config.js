e3b38dd8dadc60d9d7cae38e77b4dad67bb2f2d5
SnaveSutit <snavesutit@gmail.com>
2022-07-15 20:30:08 -0400

    Update config.js

--- datapack/config.js
+++ datapack/config.js
@@ -1,7 +1,7 @@
-// ---------------------------------------------------------
+// ---------------------------------------------------------------------
 // This template script adds a few useful arguments to the mcbuild cli:
-// -package: Build and package the world.
-// ---------------------------------------------------------
+// -package: Build and package the world, data pack, and resource pack.
+// ---------------------------------------------------------------------
 
 const { exec } = require('child_process')
 
@@ -10,8 +10,8 @@ module.exports = {
 		onBuildSuccess: (build) => {
 			if (process.argv.includes('-package')) {
 				exec(
-					`".\\package.ps1"`,
-					{ shell: 'powershell.exe' },
+					`.\\package.ps1`,
+					{ shell: 'powershell.exe', cwd: '..'},
 					(err, stdout, stderr) => {
 						if (err) {
 							console.error(err)
