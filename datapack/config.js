dfb455cf974d70e9c45b75537f72ea7c774491c0
SnaveSutit <snavesutit@gmail.com>
2022-07-15 20:18:10 -0400

    Update config.js

--- datapack/config.js
+++ datapack/config.js
@@ -1,9 +1,14 @@
+// ---------------------------------------------------------
+// This template script adds a few useful arguments to the mcbuild cli:
+// -package: Build and package the world.
+// ---------------------------------------------------------
+
 const { exec } = require('child_process')
 
 module.exports = {
 	global: {
 		onBuildSuccess: (build) => {
-			if (build.config.mc.package_after_build) {
+			if (process.argv.includes('-package')) {
 				exec(
 					`".\\package.ps1"`,
 					{ shell: 'powershell.exe' },
@@ -20,8 +25,6 @@ module.exports = {
 	},
 	mc: {
 		dev: true,
-		// Whether or not to compile the packaged version of the world after the build process is complete.
-		package_after_build: false,
 		header: '# Developed by SnaveSutit (Discord: SnaveSutit#0042 | Email: SnaveSutit@gmail.com)\n# built using mc-build (https://github.com/mc-build/mc-build)',
 		internalScoreboard: 'i',
 		generatedDirectory: 'zzz',
